136da3f9e9958dcf7cb085faf709030fd30672b0
aadamr0 <adamb.0@outlook.com>
2024-02-19 15:15:27 +0000

    3-get-api complete

--- db/app.controller.js
+++ db/app.controller.js
@@ -1,17 +1,24 @@
-const { selectTopics } = require("./app.models.js");
+const { selectTopics, selectApis } = require("./app.models.js");
 
 function getTopics(req, res, next) {
     //call select topics an send, pass any error with next
     selectTopics()
-    .then((results) => {
-        console.log(results);
-        res.status(200).send(results)
+    .then((topicsArray) => {
+        res.status(200).send(topicsArray)
     })
     .catch((err) => {
         next(err)
     })
 }
 
+function getApi(req, res, next){
+    // select from list of api endpoints json file
+    selectApis().then((apiObj) => {
+        res.status(200).send(apiObj)
+    })
+}
+
 module.exports = {
   getTopics,
+  getApi
 };
