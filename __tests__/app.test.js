2d5730777b6b8003817faac20aa419a902844255
aadamr0 <adamb.0@outlook.com>
2024-02-19 16:41:07 +0000

    4-get-articles-by-id complete

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -68,3 +68,40 @@ describe("Get: /api", () => {
       });
   });
 });
+
+describe("Get: /api/articles/:article_id", () => {
+  it("should respond with status 200 and the article requested by id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+        });
+      });
+  });
+  it("should respond with status 400 if given an invalid ID (eg of type string)", () => {
+    return request(app)
+      .get("/api/articles/seven")
+      .expect(400)
+      .then((response) => {
+        expect(response.body.msg).toBe("bad request");
+      });
+  });
+  it("should respond with status 404 if given an id outside the available range", () => {
+    return request(app)
+      .get("/api/articles/200")
+      .expect(404)
+      .then((response) => {
+        expect(response.body.msg).toBe("route does not exist");
+      });
+  });
+});
--- db/app.models.js
+++ db/app.models.js
@@ -1,24 +1,40 @@
 const fs = require("fs/promises");
-const db = require('./connection.js')
+const db = require("./connection.js");
+const pg = require("pg");
 
 function selectTopics() {
   // use psql to get data.
-  return db.query(`SELECT * FROM topics`)
-  .then((results) => {
-    return results.rows
-  })
+  return db.query(`SELECT * FROM topics`).then((results) => {
+    return results.rows;
+  });
 }
 
 function selectApis() {
-  return fs.readFile(`${__dirname}/../endpoints.json`, 'utf-8')
-  .then((results) => {
-    return JSON.parse(results)
-  })
-  .catch((err) => {
-    console.log(err)
-    return err
-  })
+  return fs
+    .readFile(`${__dirname}/../endpoints.json`, "utf-8")
+    .then((results) => {
+      return JSON.parse(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      return err;
+    });
 }
 
+function selectArticleById(article_id) {
+  return db
+    .query(
+      `SELECT * FROM articles
+  WHERE article_id = $1`,
+      [article_id]
+    )
+    .then((article) => {
+      if (article.rows.length) return article.rows[0];
+      else return Promise.reject({status: 404, msg: 'route does not exist'})
+    })
+    .catch((err) => {
+      return Promise.reject(err);
+    });
+}
 
-module.exports = { selectTopics, selectApis };
+module.exports = { selectTopics, selectApis, selectArticleById };
